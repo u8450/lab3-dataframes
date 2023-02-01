cf8bf800b6ca171b0b5ff7b5e928034952ef9c8c
u8450 <rguo3.edu>
2023-02-01 11:04:03 -0800

    updated HR question with answers

--- lab.R
+++ lab.R
@@ -1,6 +1,9 @@
 ### -------------------- Lab 3 --------------------
 ### This lab is designed to prepare you for PS3
 
+# install.packages("tidyverse")
+library(tidyverse)
+
 ## Create a vector of the number of points the Seahawks scored in the
 ## first (at least) 5 games
 ## Hint: google "Seahawks scores", or check the football database:
@@ -74,48 +77,57 @@
 ## Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee
 ## 100")
 ## Hint: use 'paste()` or `str_c`
-
+employee_num <- c(1:100) # spread operator
+employee_name <- c("Employee ")
+name <- paste(employee_name,employee_num)
 
 ## Create a random vector of their 2021 salaries.
 ## Hint: you may use the runif function to create uniform random numbers,
 ## e.g. 'runif(100, 60, 120)' creates 100 random numbers between 60 and 120
-
+salaries_2021 <- runif(100, 40, 100)
 
 ## Create a random vector of 2022 salaries that are typically
 ## higher than the 2014 salaires (use runif again).
 ## For instance, if you create random numbers between 65 and 130, then 2022
 ## salaries tend to be larger than 2021 salaries.
-
+salaries_2022 <- runif(100, 60, 110)
 
 ## Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(name,salaries_2021, salaries_2022)
+View(salaries)
 
 ## Create a column 'raise' that stores the size of the
 ## raise between 2021 and 2022
-
+salaries$raise <- salaries_2022 - salaries_2021
 
 ## Retrieve values from your data frame to answer the following questions:
 ##
-## What was the 2015 salary of employee 57
-
+## What was the 2021 salary of employee 57
+em57_salries2021 <- salaries[salaries$name == "Employee  57", "salaries_2021"]
 
 ## Now round the answer down to two digits after comma
 ## check out 'round()' function
-
+rounded_salary <- round(em57_salries2021, 3)
 
 ## How many employees got a raise?
-
+nrow(salaries[salaries$raise > 0, ])
 
 ## What was the value of the highest raise?
 ## Round the number to two digits!
-
-
+max_raise <- max(salaries$raise)
+round_max_raise <- round(max(salaries$raise),2)
+  
+  
 ## What was the name of the employee who recieved the highest raise?
-
+salaries[salaries$raise == max_raise, "name"]
 
 ## What was the average salary increase?
 ## Round the number!
-
+avg_increase <- mean(salaries[salaries$raise > 0, "raise"])
+round_avg_increase <- round(avg_increase, 2)
 
 ## For people who did not get a raise, how much money did they lose?
 ## Round the number!
+paycut <- salaries[salaries$raise < 0, "raise"]
+
+
